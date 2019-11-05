da6f928a099feb830dd14a9907100fffa3d7ca08
hunterhann2010 <josh.m.hunter@gmail.com>
2019-11-05 12:24:06 -0800

    more boilerplate

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const expressJWT = require('express-jwt');
+
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log(`Connected to mongoDB on ${db.host}: ${db.port}...`);
+});
+db.on('error', (err) => {
+    console.log(`Database error:\n${err}`);
+});
+
+
+app.use('/auth', require('./routes/auth'));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Your are listening to station port${process.env.PORT}...`);
+});
