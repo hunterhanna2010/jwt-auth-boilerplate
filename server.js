9302ed44074c85ce63a71e0a6cbb952404fd398f
hunterhann2010 <josh.m.hunter@gmail.com>
2019-11-05 16:27:45 -0800

    finished boilerplate with token auth

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const expressJWT = require('express-jwt');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -20,7 +21,10 @@ db.on('error', (err) => {
 
 
 app.use('/auth', require('./routes/auth'));
+app.use('/locked',
+    expressJWT({ secret: process.env.JWT_SECRET }).unless({ method: 'POST' }), 
+    require('./routes/locked'));
 
 app.listen(process.env.PORT, () => {
-    console.log(`Your are listening to station port${process.env.PORT}...`);
+    console.log(`You are listening to station port ${process.env.PORT}...`);
 });
